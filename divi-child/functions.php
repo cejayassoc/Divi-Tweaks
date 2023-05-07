af6f288ab79315f8b0346c2bafc1d664b1659fbe
Lisa Baird <design@cejay.com>
2023-05-07 16:46:00 -0400

    Update functions.php
    Remove meta keywords - no longer valid meta.  Add og for site name type

--- divi-child/functions.php
+++ divi-child/functions.php
@@ -88,11 +88,13 @@ function add_allowed_origins( $origins ) {
 /* or the default title keyword set in this function is used */
 
 function cejay_magic_meta() {
+global $post;
 $metas= get_post_custom(get_the_ID());
+
 	//Set defaults:
+	$post_thumbnail_id = get_post_thumbnail_id( $post );
 	$title="";
 	$description= "";
-	$keywords="" ;
 	$imageURL="" ;
 
 
@@ -108,21 +110,18 @@ if(isset($cstmMeta['cejay-description'][0]) && $cstmMeta['cejay-description'][0]
 	elseif (has_excerpt()) // Use post excerpt if no custom meta
 		{$description= strip_tags( get_the_excerpt() );}
 
-/**KEYWORDS**/    
-  if($cstmMeta['cejay-keywords'][0]!='')
-       {$keywords=$cstmMeta['cejay-keywords'][0];}	
-/*** Featured Image for Open Graph Meta **/
-	global $post;
-    	$post_thumbnail_id = get_post_thumbnail_id( $post );
-	echo '<meta property="og:image" content="'; wp_get_attachment_image_url( $post_thumbnail_id, $size ); echo $imageURL.'" />';echo "\r\n";     
+/**KEYWORDS - NOT USED, GOOGLE IGNORES THEM AND OTHER BROWSERS CONSIDER IT SPAM.**/      
 	
-	echo '<meta property="og:type" content="article" />'; echo "\r\n";
-	echo '<meta name="title" content="'.$title.'" />'; echo "\r\n";
-	echo '<meta property="og:title" content="'.$title.'" />';echo "\r\n";
-	echo '<meta property="og:url"  content="'; the_permalink(); echo '" />';echo "\r\n";
 	echo '<meta name="description" content="'.$description.'" />';echo "\r\n";
+	echo '<meta name="title" content="'.$title.'" />'; echo "\r\n";
+	
+	echo '<meta property="og:title" content="'.$title.'" />';echo "\r\n";  //Text that appears in preview
+	echo '<meta property="og:type" content="website" />';echo "\r\n"; //or audio, video, etc
+	echo '<meta property="og:url"  content="'; the_permalink(); echo '" />';echo "\r\n"; //link to this page or article
+	echo '<meta property="og:image" content="'; wp_get_attachment_image_url( $post_thumbnail_id, $size ); echo $imageURL.'" />';echo "\r\n"; 
 	echo '<meta property="og:description" content="'.$description.'" />';echo "\r\n";
-	echo '<meta name="keywords" content="'.$keywords.'" />';echo "\r\n";
+	echo '<meta prooperty="og:local" content="en_us" />';echo "\r\n";
+	echo '<meta property="og:site_name" content="'.get_bloginfo('name').'" />';echo "\r\n";
 	
 
 }
